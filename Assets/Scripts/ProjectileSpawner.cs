0ad6d07b351261aa49103047042e48dd6a75a6c5
Matthew Holliday <matthewjacobholliday@gmail.com>
2022-05-15 13:00:51 -0500

    Right-Hand projectiles firing.

---
+++ Assets/Scripts/ProjectileSpawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ProjectileSpawner : MonoBehaviour
+{
+    [SerializeField] private InputActionReference shootRightHandActionReference;
+    public Rigidbody projectile;
+    private float speed = 20.0f;
+
+    void Start()
+    {
+        shootRightHandActionReference.action.performed += OnShoot;
+    }
+
+    private void OnShoot(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation);
+        instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
+    }
+}
